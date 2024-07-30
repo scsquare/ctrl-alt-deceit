0c12668f15d79707107caa2b229a10ca82c5781b
stephaniecho <stephanie.cho33@gmail.com>
2024-07-30 18:30:54 +0800

    Actually here

---
+++ js/dosomething.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("code1")
+
+button.addEventListener("click",()=>{
+    button.innerHTML=String.fromCharCode(35,74,117,110,101,77,101,101,116,115,79,110,105,111,110);
+})
+//Developer notes: #It’sTorNotOnion
